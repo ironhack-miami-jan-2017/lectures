929242bc4c7f11844e7a2ba4dcec2a862e395d05
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-02-24 10:55:39 -0500

    Add Cat Mongoose example

---
+++ week4/day5/mongoose-example/example.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/exampleApp');
+  //                                       |
+  //                                use exampleApp
+
+
+const Cat = mongoose.model('Cat', { name: String });
+  //   |
+  //   --------------------
+  //                      |
+  // interacts with the "cats" collection
+
+
+const kitty = new Cat({ name: 'Ginger' });
+
+  //  db.cats.insertOne({ name: 'Ginger' })
+  //              |
+  //   ------------
+  //   |
+kitty.save((err) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log(`meow ${kitty._id}`);
+});
+
+
+  // db.cats.find({}, { _id: 0, name: 1 })
+  //           |
+  //  ----------
+  //  |
+Cat.find({}, { _id: 0, name: 1 }, (err, cats) => {
+  if (err) {
+    throw err;
+  }
+
+  cats.forEach((oneCat) => {
+    console.log(` --> cat: ${oneCat.name} ${oneCat._id}`);
+  });
+});
